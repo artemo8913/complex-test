6df11700af97187459e415751d4ebb191f47882c
Spiryaev Artem <kaline-son@ya.ru>
2025-06-21 17:55:04 +0700

    upd: productCard

--- src/2entities/product/ui/ProductCard.tsx
+++ src/2entities/product/ui/ProductCard.tsx
@@ -2,9 +2,10 @@
 import clsx from "clsx";
 import { JSX } from "react";
 
+import { ImageWithFallback } from "@/1shared/ui/ImageWithFallback";
+
 import style from "./ProductCard.module.css";
 import { Product } from "../model/product.type";
-import Image from "next/image";
 
 interface ProductCardProps {
   product: Product;
@@ -15,7 +16,7 @@ interface ProductCardProps {
 export function ProductCard(props: ProductCardProps) {
   return (
     <div className={clsx(style.ProductCard, props.className)}>
-      <Image alt={props.product.title} src={props.product.image_url} fill unoptimized />
+      <ImageWithFallback className={style.image} alt={props.product.title} src={props.product.image_url} />
       <p className={style.title}>{props.product.title}</p>
       <p className={style.description}>{props.product.description}</p>
       <p className={style.price}>Цена: {props.product.price}₽</p>
